cd843bb0b079e4d2e88962858682eb3d0092606b
Mattia Simonato <mattiasimonato.w@gmail.com>
2018-10-06 08:47:12 +0200

    Create allergies component markup and style

---
+++ src/components/Allergies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Allergies = (props) => {
+  return (
+    <ul className="allergies">
+      <li className="allergies__item allergies__item--asterisk">allergy</li>
+    </ul>
+  )
+};
+
+export default Allergies;
