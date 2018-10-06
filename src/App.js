164fe74d73491204f6bf5696925dea260f276e0a
Mattia Simonato <mattiasimonato.w@gmail.com>
2018-10-06 21:35:30 +0200

    use mapStateToProps to receive data from the store and pass data to childs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Header from './components/Header.js'
-import PageTitle from './containers/PageTitle.js'
+import PageTitle from './components/PageTitle.js'
 import MainContainer from './components/MainContainer.js'
 import Menu from './components/Menu.js'
 import Footer from './containers/Footer.js'
@@ -10,17 +11,38 @@ import './styles/App.scss';
 
 class App extends Component {
   render() {
+
+    const courses = this.props.courses.filter((course) => {
+      if (course.courseType.indexOf(this.props.courseTypeSelected) >= 0) {
+        return course
+      }
+      return null
+    })
+
+    const courseTitle = this.props.courseTypes[this.props.courseTypeSelected]
+
     return (
       <div className="App">
         <Header chef="Mattia Simonato"/>
-        <PageTitle/>
+        <PageTitle courseTitle={courseTitle}/>
         <MainContainer>
-          <Menu/>
+          <Menu courses={courses}/>
         </MainContainer>
-        <Footer/>
+        <Footer/>s
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    courseTypeSelected: state.courseTypeSelected,
+    courses: state.courses,
+    courseTypes: state.courseTypes
+  }
+}
+
+
+
+
+export default connect(mapStateToProps, null)(App);
