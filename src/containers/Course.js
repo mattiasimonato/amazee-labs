312d9a898865f094bf49f9e57560bfd3b02a1729
Mattia Simonato <mattiasimonato.w@gmail.com>
2018-10-06 08:47:41 +0200

    Create course component markup and style

---
+++ src/containers/Course.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Allergies from '../components/Allergies.js'
+
+class Course extends Component {
+  render() {
+    return (
+      <div className="course">
+        <div className="course__wrap-img">image</div>
+        <div className="course__content">
+          <h5 className="course__name">Mixed Greens with Poached Eggs, Hazelnuts and Spices</h5>
+          <p className="course__description">What could be more summery than grilled peppers and squash heaped on toasted country bread that's slathered with fresh basil pesto?</p>
+
+          <Allergies/>
+        </div>
+        <div className="course__wrap-icon">check</div>
+      </div>
+    );
+  }
+}
+
+export default Course;
