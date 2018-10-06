3313ca77322af913e15654c9c3b894438ecfedc2
Mattia Simonato <mattiasimonato.w@gmail.com>
2018-10-06 08:48:00 +0200

    Create menu component markup and style

---
+++ src/components/Menu.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Course from '../containers/Course.js'
+
+const Menu = (props) => {
+  return (
+    <ul className="menu">
+      <li className="menu__item">
+        <Course/>
+      </li>
+      <li className="menu__item">
+        <Course/>
+      </li>
+      <li className="menu__item">
+        <Course/>
+      </li>
+      <li className="menu__item">
+        <Course/>
+      </li>
+      <li className="menu__item">
+        <Course/>
+      </li>
+      <li className="menu__item">
+        <Course/>
+      </li>
+      <li className="menu__item">
+        <Course/>
+      </li>
+      <li className="menu__item">
+        <Course/>
+      </li>
+      <li className="menu__item">
+        <Course/>
+      </li>
+    </ul>
+  )
+};
+
+export default Menu;
