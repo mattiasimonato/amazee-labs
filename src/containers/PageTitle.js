37dd20acfb1d7413599c9133c354e169d9aed983
Mattia Simonato <mattiasimonato.w@gmail.com>
2018-10-06 08:45:32 +0200

    Create page-title component markup and style

---
+++ src/containers/PageTitle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PageTitle extends Component {
+  render() {
+    return (
+      <section className="page-title text-center">
+        <h2 className="page-title__primary">à la carte menu</h2>
+        <h4 className="page-title__secondary">hors d'ouvres</h4>
+      </section>
+    );
+  }
+}
+
+export default PageTitle;
