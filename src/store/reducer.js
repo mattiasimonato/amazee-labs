1f9ff265061e2bd64a9d4459b59f6eab99de7217
Mattia Simonato <mattiasimonato.w@gmail.com>
2018-10-06 15:58:34 +0200

    Add redux and react redux to the project

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionType from './actionType'
+
+const initialState = {
+  courseType: 0,
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default : return state;
+  }
+}
+export default reducer
