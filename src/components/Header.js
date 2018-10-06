4ac9462cdec5849de377cec9315170cfe7a5cff4
Mattia Simonato <mattiasimonato.w@gmail.com>
2018-10-06 08:45:03 +0200

    Create header component markup and style

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../images/amazee-labs-logo.svg';
+
+const Header = (props) => {
+  return (
+    <header className="header container">
+      <div className="row align-items-center">
+        <div className="header__logo col-md-6">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="header__logo col-md-6 text-right">
+          <span className="header__label">Chef:</span>
+          <strong>{props.chef}</strong>
+        </div>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
